5008015333a2b6f25ed2ad1ee6fc0240d5082bf6
karber <karl.berglof@hotmail.com>
2019-08-28 11:15:37 +0200

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Header headerText="Hello Gatsby!" />
+    <Header headerText="Hello Karl!" />
     <Link to="/contact/">Contact</Link>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
